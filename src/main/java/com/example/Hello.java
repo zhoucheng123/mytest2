9c2644d7094df6d0804c6e46ba15e6f61a2c430f
周城 <924301080@qq.com>
2021-09-11 15:11:28 +0800

    添加了一个test函数

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
     static int  add(int b ,int a){
         return a+b;
     }
+    static int cheng(int a,int b){
+        return a*b;
+    }
     public static void main(String[] args){
         System.out.println("Hello");
     }
