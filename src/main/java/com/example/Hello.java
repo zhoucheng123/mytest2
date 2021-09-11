8da6bf21a1939436ff96e9fe3f5f57ac097ea706
周城 <924301080@qq.com>
2021-09-11 14:43:31 +0800

    添加了一个add函数

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,6 +1,9 @@
 package com.example;
 
 public class Hello {
+    static int  add(int b ,int a){
+        return a+b;
+    }
     public static void main(String[] args){
         System.out.println("Hello");
     }
